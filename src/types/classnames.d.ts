b6ff6caf57d654ac0cfe9f659a7cee67ce606fe0
Jitto Joseph <78639807+JittoJoseph@users.noreply.github.com>
2024-12-19 16:51:50 +0530

    classnames error patch

--- src/types/classnames.d.ts
+++ src/types/classnames.d.ts
@@ -1,3 +1,8 @@
 declare module 'classnames' {
-	export default function classNames(...args: any[]): string;
+	type ClassValue = string | number | ClassDictionary | ClassValue[] | undefined | null | boolean;
+	interface ClassDictionary {
+		[id: string]: string | number | boolean | undefined | null;
+	}
+
+	export default function classNames(...args: ClassValue[]): string;
 }
